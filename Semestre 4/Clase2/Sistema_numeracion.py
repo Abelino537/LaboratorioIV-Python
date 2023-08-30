e154b28790e3df4d5b65ad4a10f237a70cd4b10f
Abel Pierna <103205401+Abelino537@users.noreply.github.com>
2023-08-30 16:29:25 -0300

    Clase 2

---
+++ Semestre 4/Clase2/Sistema_numeracion.py
@@ -0,0 +1,50 @@
+"""
+UTN
+Tec. Programacion
+Laboratorio IV
+Python
+Clase 2
+28/08/23
+Abel Pierna
+"""
+
+# Profundizando en los sistemas de numeración
+
+# Sistema Decimal
+a = 10
+print(f'a Decimal: {a}')
+
+# Sistema Binario
+a = 0b1010
+print(f'a Binario: {a}')
+
+# Sistema Octal
+a = 0o12
+print(f'a Octal: {a}')
+
+# Sistema Hexadecimal
+a = 0xA
+print(f'a Hexadecimal: {a}')
+
+# Conversión de valores
+
+# Base Decimal
+a = int('23', 10);
+print(f'a base Decimal: {a}')
+
+# Base Binario
+a = int('10111', 2)
+print(f'a base Binario: {a}')
+
+# Base Octal
+a = int('27', 8)
+print(f'a base Octal: {a}')
+
+# Base Hexadecimal
+a = int('17', 16)
+print(f'a base Hexadecimal: {a}')
+
+# Base 5 de 0 a 4
+a = int('43', 5)
+print(f'a base 5: {a}')
+
